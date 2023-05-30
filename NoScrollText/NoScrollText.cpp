6fb896ec378be2815ef0ad8dea86aaf67fa0db6e
hebiiro <96464759+hebiiro@users.noreply.github.com>
2023-05-30 15:49:40 +0900

    1.0.0
    初版

---
+++ NoScrollText/NoScrollText.cpp
@@ -0,0 +1,92 @@
+﻿#include "pch.h"
+
+//--------------------------------------------------------------------
+
+// デバッグ用コールバック関数。デバッグメッセージを出力する。
+void ___outputLog(LPCTSTR text, LPCTSTR output)
+{
+	::OutputDebugString(output);
+}
+
+//--------------------------------------------------------------------
+
+DECLARE_HOOK_PROC(BOOL, WINAPI, drawObjectText, (HDC dc, int x, int y, UINT options, LPCRECT rc, LPCSTR text, UINT c, CONST INT* dx));
+IMPLEMENT_HOOK_PROC_NULL(BOOL, WINAPI, drawObjectText, (HDC dc, int x, int y, UINT options, LPCRECT rc, LPCSTR text, UINT c, CONST INT* dx))
+{
+	MY_TRACE(_T("drawObjectText(0x%08X, %d, %d, 0x%08X)\n"), dc, x, y, options);
+	MY_TRACE_RECT2(rc[0]); // クリッピング矩形
+	MY_TRACE_RECT2(rc[1]); // アイテム全体の矩形
+
+	x = std::max(70, x);
+
+	return true_drawObjectText(dc, x, y, options, rc, text, c, dx);
+}
+
+//--------------------------------------------------------------------
+
+BOOL func_init(AviUtl::FilterPlugin* fp)
+{
+	MY_TRACE(_T("func_init()\n"));
+
+	uintptr_t exedit = (uintptr_t)::GetModuleHandle(_T("exedit.auf"));
+	MY_TRACE_HEX(exedit);
+
+	true_drawObjectText = hookAbsoluteCall(exedit + 0x0003794B, hook_drawObjectText);
+	MY_TRACE_HEX(true_drawObjectText);
+
+	return TRUE;
+}
+
+BOOL func_exit(AviUtl::FilterPlugin* fp)
+{
+	MY_TRACE(_T("func_exit()\n"));
+
+	return TRUE;
+}
+
+BOOL APIENTRY DllMain(HINSTANCE instance, DWORD reason, LPVOID reserved)
+{
+	switch (reason)
+	{
+	case DLL_PROCESS_ATTACH:
+		{
+			MY_TRACE(_T("DLL_PROCESS_ATTACH\n"));
+
+			break;
+		}
+	case DLL_PROCESS_DETACH:
+		{
+			MY_TRACE(_T("DLL_PROCESS_DETACH\n"));
+
+			break;
+		}
+	}
+
+	return TRUE;
+}
+
+//--------------------------------------------------------------------
+
+AviUtl::FilterPluginDLL* WINAPI GetFilterTable()
+{
+	LPCSTR name = "アイテム名常時表示";
+	LPCSTR information = "アイテム名常時表示 1.0.0 by 蛇色";
+
+	static AviUtl::FilterPluginDLL filter =
+	{
+		.flag =
+			AviUtl::FilterPluginDLL::Flag::AlwaysActive |
+			AviUtl::FilterPluginDLL::Flag::DispFilter |
+			AviUtl::FilterPluginDLL::Flag::ExInformation,
+		.x = 0,
+		.y = 0,
+		.name = name,
+		.func_init = func_init,
+		.func_exit = func_exit,
+		.information = information,
+	};
+
+	return &filter;
+}
+
+//--------------------------------------------------------------------
---
+++ NoScrollText/pch.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#include <windows.h>
+#include <shlwapi.h>
+#pragma comment(lib, "shlwapi.lib")
+#include <mmsystem.h>
+#pragma comment(lib, "winmm.lib")
+
+#include <tchar.h>
+#include <strsafe.h>
+
+#include "AviUtl/aviutl_exedit_sdk/aviutl.hpp"
+#include "AviUtl/aviutl_exedit_sdk/exedit.hpp"
+#include "Common/Tracer.h"
+#include "Common/Hook.h"
